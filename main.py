acc75a7d227c46ca124895167320c163d97a382c
Дмитрий Артёменко <80966196+Grade-collab@users.noreply.github.com>
2021-11-02 18:36:49 +0300

    Add new function

--- main.py
+++ main.py
@@ -9,13 +9,14 @@ bot = telebot.TeleBot(token)
 def start_message(message):
     keyboard = telebot.types.ReplyKeyboardMarkup(True)
     keyboard.row('Расскажи о себе!', 'Как с тобой связаться?', '')
+    keyboard.row('Как найти тебя в соц.сетях?')
     bot.send_message(message.chat.id, 'Здравствуйте! Я - Дима Артёменко.', reply_markup=keyboard)
     @bot.message_handler(content_types=['text'])
     def send_text(message):
         if message.text.lower() == 'расскажи о себе!':
             bot.send_message(message.chat.id, 'Я - просто школьник из Новочеркасска:) Зовут Димой Артёменко. Ну или же просто Артём. Вместе со своей командой увлекаюсь кодингом) Пишу на много чём, но мой основное направление - WEB-разработка. Мне 13 и в свой возраст - я просто нарабатываю портфолио!')
         if message.text.lower() == 'как с тобой связаться?':
-            bot.send_message(message.chat.id, 'Очень просто:) Я всегда открыт для общения! Пишите @Suicidessss                                                                                    Звоните: +7 (938) 144-42-90')
-        if message.text.lower() == 'Тут арбуз':
-            bot.send_message(message.chat.id, 'И тут')
+            bot.send_message(message.chat.id, 'Очень просто:) Я всегда открыт для общения! Пишите @Suicidessss')
+        if message.text.lower() == 'как найти тебя в соц.сетях?':
+            bot.send_message(message.chat.id, 'Instagram: @a.r.t.i.k_dimovich VK: @rioar Telegram: @Suicidessss')
 bot.polling()
